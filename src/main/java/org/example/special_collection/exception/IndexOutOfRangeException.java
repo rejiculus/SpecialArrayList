59f508d7d89948780a92a8c33b14633a202e7813
Bezdomnii <nik.bezdomnii@gmail.com>
2024-09-02 15:36:23 +0300

    add new constructor to IndexOutOfRangeException
    used when checking an index for a value less than zero

--- src/main/java/org/example/special_collection/exception/IndexOutOfRangeException.java
+++ src/main/java/org/example/special_collection/exception/IndexOutOfRangeException.java
@@ -4,4 +4,7 @@ public class IndexOutOfRangeException extends RuntimeException{
     public IndexOutOfRangeException(int capacity, int index) {
         super(String.format("Index out of rang! It must be in range from 0 to %d (include). Your index = %d",capacity-1,index));
     }
+    public IndexOutOfRangeException(int index) {
+        super(String.format("Index out of rang! It must be more than zero! Your index = %d",index));
+    }
 }
